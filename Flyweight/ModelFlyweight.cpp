60d4c25ea50df51f1816f418bf52c33eef3d33c2
Lin Li <lilin.obasic@gmail.com>
2024-09-03 18:01:55 +0200

    Flyweight initial commit

---
+++ Flyweight/ModelFlyweight.cpp
@@ -0,0 +1,8 @@
+/**
+ * Flyweight Design Pattern
+ * 
+ * In this example, a 3D scene creation software is shown. It can create thousands of objects such as trees, 
+ * houses, etc. In order to reduce memeory usage, the common (intrisic) attributes of the objects are modeled 
+ * as Flyweight, such as color, texture. The individual (extrinsic) attributes are modeled as unique states, 
+ * such as position, size, etc.
+ */
